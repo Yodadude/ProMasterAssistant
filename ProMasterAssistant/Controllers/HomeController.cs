88b205ae0730e5650f157970b1b90cfb59ba410e
John Byrne <john.byrne.oz@gmail.com>
2013-11-30 23:24:16 +1100

    Removed Entity Framework

--- ProMasterAssistant/Controllers/HomeController.cs
+++ ProMasterAssistant/Controllers/HomeController.cs
@@ -11,10 +11,8 @@ namespace ProMasterAssistant.Controllers
 	[MainMenu]
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-		ConnectionsEntities db = new ConnectionsEntities();
 
+        // GET: /Home/
         public ActionResult Index()
         {
 			//List<UserConnectionString> connStrings = db.UserConnectionStrings.Where(x => x.UserName == User.Identity.Name).ToList();
@@ -24,48 +22,12 @@ namespace ProMasterAssistant.Controllers
         }
 
 		[HttpPost]
-		public JsonResult Add(string connName, string serverName, string databaseName, string userId, string password)
-		{
-			//try
-			//{
-			//    db.UserConnectionStrings.AddObject(
-			//        new UserConnectionString { UserName = User.Identity.Name, ConnectionName = connName, ConnectionString = connString });
-			//    db.SaveChanges();
-			//}
-			//catch (Exception e)
-			//{
-
-			//    return Json(new { status = "error", message = e.Message });
-			//}
-
-			return Json(new { status = "ok", message = "" });
-		}
-
-		[HttpPost]
 		public JsonResult SetConnection(ConnectionSettingsModel model)
 		{
 
 			try
 			{
-				//string connectionString = new System.Configuration.ConfigurationSettings.AppSettings["ConnectionString"]);
-
-				var scsb = new System.Data.SqlClient.SqlConnectionStringBuilder
-				{
-					DataSource = model.Server,
-					InitialCatalog = model.Database,
-					UserID = model.UserId,
-					Password = model.Password
-				};
-
-				var ecb = new EntityConnectionStringBuilder
-				{
-					Metadata = "res://*/Models.ProMaster.csdl|res://*/Models.ProMaster.ssdl|res://*/Models.ProMaster.msl",
-					Provider = "System.Data.SqlClient",
-					ProviderConnectionString = scsb.ConnectionString
-				};
-
-				var dbpm = new ProMasterEntities(ecb.ConnectionString);
-				//var dbpm = new ProMasterEntities("data source="+model.Server+";initial catalog="+model.Database+";User Id="+model.UserId+";Password="+model.Password);
+								
 			}
 			catch (Exception e)
 			{
--- ProMasterAssistant/Models/ConnectionSettingsModel.cs
+++ ProMasterAssistant/Models/ConnectionSettingsModel.cs
@@ -5,12 +5,11 @@ using System.Web;
 
 namespace ProMasterAssistant.Models
 {
-	public class ConnectionSettingsModel
-	{
-		public string ConnectionName { get; set; }
-		public string Server { get; set; }
-		public string Database { get; set; }
-		public string UserId { get; set; }
-		public string Password { get; set; }
-	}
+    public class ConnectionSettingsModel
+    {
+        public string Server { get; set; }
+        public string Database { get; set; }
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
 }
